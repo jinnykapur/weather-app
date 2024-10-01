e712842c9a267ad06506fa88cd5301e47273d02e
JINNY <114975884+jinnykapur@users.noreply.github.com>
2024-10-01 06:08:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+let weather = {
+    apiKey: "67b92f0af5416edbfe58458f502b0a31",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+});
+
+weather.fetchWeather("Kharar");
+
+// Background image carousel
+const images = [
+  'https://source.unsplash.com/1600x900/?nature,water',
+  'https://source.unsplash.com/1600x900/?mountains,forest',
+  'https://source.unsplash.com/1600x900/?sky,clouds',
+  'https://source.unsplash.com/1600x900/?city,night'
+];
+
+let index = 0;
+
+function changeBackground() {
+  document.body.style.backgroundImage = `url('${images[index]}')`;
+  index = (index + 1) % images.length;  // Loop through images array
+}
+
+setInterval(changeBackground, 2000); // Change image every 2 seconds
